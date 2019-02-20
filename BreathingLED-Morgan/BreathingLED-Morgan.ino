de28e4669dd01f55b608c116ce857631e78a6c7b
Morgan Sholeen <msholeen@utexas.edu>
2019-02-20 17:35:26 -0600

    Part 5

---
+++ BreathingLED-Morgan/BreathingLED-Morgan.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void dimmer(int freq, int duty){
+  int period, onTime, offTime;
+  period = 1000/freq; //delay() in milliseconds
+  onTime = period * duty/100; //how to pretend duty is a fraction
+  offTime = period - onTime; //all time is accounted for (none lost to rounding)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(offTime);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  int i = 0; 
+  while(i <= 100) {
+    dimmer(100, i);
+    i++;
+  }
+  while(i >= 0) {
+    dimmer(100, i);
+    i--;
+  }
+
+
+}
