b24fec3c37b747bd940955edb5a5ab3332b85e66
Anjie Shen <anjieshen0930@utexas.edu>
2019-02-20 19:34:59 -0600

    Almost finished lab 1

--- Arduino_Lab_1_anjie/Lab1_Part_4/Lab1_Part_4.ino
+++ Arduino_Lab_1_anjie/Lab1_Part_4/Lab1_Part_4.ino
@@ -1,18 +1,32 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
 }
+
 void dimmer(int freq, int duty) {
   int period, onTime, offTime;
-  period = 1000/freq; //delay is in ms
-  onTime = period * duty/100; 
+  int Hz=100;
+  period = 1000/freq;     //delay is in ms
+  onTime = period * duty/Hz; 
   offTime = period - onTime;  
   digitalWrite(LED_BUILTIN, HIGH);
   delay(onTime);
   digitalWrite(LED_BUILTIN, LOW);
   delay(offTime);
+  Hz = Hz - 10;
   }
+
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int i=0;
+  int j=100;
+  while (j>0){
+    for (i=1; i<=100; i++) {
+        dimmer(j, i);
+      }
+    for (;i>=0;i--) {
+        dimmer(100, i);
+      }
+    j = j-10;
+   }
 }
